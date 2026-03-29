39577394a8b4eb79cda1af054b4be7118b8f10d3
oki0ki <oki0ki@hotmail.com>
2026-03-29 04:47:49 +0200

    Update console/start.sh

--- console/start.sh
+++ console/start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+export PORT=${PORT:-10000}
+
+envsubst '${PORT}' < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
+
 API_PORT=3001 /app/gateway &
+PORT=3000 node /app/svelte/build &
+
 nginx -g 'daemon off;'
