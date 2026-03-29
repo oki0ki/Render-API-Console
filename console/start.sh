5baba4431e7dee113a9e7904c41c42d6aa1f33d3
oki0ki <oki0ki@hotmail.com>
2026-03-29 05:57:56 +0200

    Remove nginx, Go proxies to SvelteKit: console/start.sh

--- console/start.sh
+++ console/start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 export PORT=${PORT:-10000}
 
-envsubst '${PORT}' < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
-
-API_PORT=3001 /app/gateway &
 PORT=3000 node /app/svelte/build &
 
-nginx -g 'daemon off;'
+exec /app/gateway
