ad0594a63ac3aa7490a6b8adc21c4c3242882222
oki0ki <oki0ki@hotmail.com>
2026-03-29 06:18:06 +0200

    Remove start.sh (combined service cleanup)

--- console/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export PORT=${PORT:-10000}
-
-PORT=3000 node /app/svelte/build &
-SVELTE_PID=$!
-
-( wait $SVELTE_PID; echo "SvelteKit exited, shutting down container"; kill 1 ) &
-
-exec /app/gateway
