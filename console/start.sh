6fb964aa1d973e3bc133b16e923512859262218a
oki0ki <oki0ki@hotmail.com>
2026-03-29 06:01:00 +0200

    Add fail-fast: restart container if SvelteKit dies

--- console/start.sh
+++ console/start.sh
@@ -2,5 +2,8 @@
 export PORT=${PORT:-10000}
 
 PORT=3000 node /app/svelte/build &
+SVELTE_PID=$!
+
+( wait $SVELTE_PID; echo "SvelteKit exited, shutting down container"; kill 1 ) &
 
 exec /app/gateway
